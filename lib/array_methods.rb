6630a77c266d7e2cfd68c92e0421792d93a161d3
Tanya Bihag <tbihag@gmail.com>
2019-08-08 19:45:42 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -3,9 +3,10 @@ def find_element_index(array, value_to_find)
   puts find_element_index[index]
 }
 
-def find_max_value(array)
-  # Add your solution here
-end
+def find_max_value [1,2,1,3,4,3,5,4,3,2,1]
+ find_max_value { |max|
+  puts find_max_value[max]
+}
 
 def find_min_value(array)
   # Add your solution here
